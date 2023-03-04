e45a82ab3296e96cc7c059b37a175f07a4a784ef
YaAlBagdattig379 <rabiulmiah54@gmail.com>
2023-03-04 12:25:20 +0530

    m67 v8 side Effect are solved such as aftr clck pagination btn got error on Order summery

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import UseCart from '../../Hooks/UseCart';
 import useProducts from '../../Hooks/UseProducts';
 import { addToDb, getStoredCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
@@ -7,7 +8,7 @@ import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = UseCart();
     // for pagination
     const [pageCount,setPageCount] = useState(0);
     const [page,setPage] = useState(0);
@@ -30,20 +31,7 @@ const Shop = () => {
              setPageCount(pages);
         })
     },[])
-    useEffect( () =>{
-        const storedCart = getStoredCart();
-        const savedCart = [];
-        for(const id in storedCart){
-            const addedProduct = products.find(product => product._id === id);
-            if(addedProduct){
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                savedCart.push(addedProduct);
-            }
-        }
-        setCart(savedCart);
-    }, [products])
-
+    
     const handleAddToCart = (selectedProduct) =>{
         //console.log(selectedProduct);
         let newCart = [];
@@ -79,11 +67,11 @@ const Shop = () => {
                      .map(number => <button
                      className={page===number? "selected":''}
                      onClick={()=>setPage(number)}
-                     >{number + 1}</button>)
+                     >{number}</button>)
                   }
                   <select onChange={e=>setSize(e.target.value)} name="" id="">
                         <option value="5">5</option>
-                        <option value="10"selected>10</option>
+                        <option value="10">10</option>
                         <option value="15">15</option>
                         <option value="20">20</option>
                   </select>
